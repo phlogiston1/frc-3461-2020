45bc78257a7c5c77b8d57689ab1e5467a44ad474
Phlogiston1 <sdbazoes@gmail.com>
2020-02-14 10:59:31 -0500

    added comments

--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -10,65 +10,140 @@ public class Limelight {
   private final double tHeight;
   private final double cHeight;
 
+  /**
+   * 
+   * @param targetHeight the height of the target for calculating distance
+   * @param cameraHeight the height of the camera for calculating distance
+   */
   public Limelight(double targetHeight, double cameraHeight){
     tHeight = targetHeight;
     cHeight = cameraHeight;
   }
+
+  /**
+   * get a raw table entry from the limelight networktables.
+   * @param entry the name of the entry to get
+   * @return a double with the value of the entry.
+   */
   public double limelightTableValue(String entry){
     return NetworkTableInstance.getDefault().getTable("limelight").getEntry(entry).getDouble(0.0);
   }
+  /**
+   * set a raw limetlight networktables value.
+   * @param entry the name of the entry
+   * @param value the value to set it to.
+   */
   public void writeTableValue(String entry, double value){
     NetworkTableInstance.getDefault().getTable("limelight").getEntry(entry).setNumber(value);
   }
+  /**
+   * get the distance to the target using the targets apparent angle from the camera and the height of the target and camera.
+   * @return the distance in the units the camera height and target height were defined in.
+   */
   public double getTargetDistance(){
     double targetOffsetY = getTargetOffsetY();
     double distance = 0;
     distance = (tHeight - cHeight) / Math.tan(Math.toRadians(Constants.CAMERA_ANGLE) + Math.toRadians(targetOffsetY));
     return distance;
   }
+  /**
+   * get the targets x offset from the camera
+   * @return the angle as a double from -27 to 27 degrees
+   */
   public double getTargetOffsetX(){
     return limelightTableValue("tx"); //-27 to 27 degrees
   }
+  /**
+   * get the targets y offset from the camera
+   * @return the angle in degrees.
+   */
   public double getTargetOffsetY(){
     return limelightTableValue("ty");
   }
+  /**
+   * the rotation of the fitted bounding box.
+   * @return the skew in degrees
+   */
   public double skew(){
     return limelightTableValue("ts");
   }
+  /**
+   * get the length of the shortest side of the fitted bounding box.
+   * @return the length in pixels.
+   */
   public double getTargetShortSidelength(){ //pixels
     return limelightTableValue("tshort");
   }
+  /**
+   * get the length of the longest side of the fitted bounding box.
+   * @return the length in pixels
+   */
   public double getTargetLongSidelength(){ //pixels
     return limelightTableValue("tlong");
   }
+  /**
+   * get the width of the rough bounding box
+   * @return the width in pixels from 0 - 320
+   */
   public double getRoughWidth(){ //0-320 pixels
     return limelightTableValue("thor");
   }
+  /**
+   * get the height of the rough bounding box
+   * @return the height in pixels from 0 - 320
+   */
   public double getRoughHeight(){ //0 - 320 pixels
     return limelightTableValue("tvert");
   }
+  /**
+   * whether the limelight is tracking the target
+   * @return boolea. true if the limelight has target.
+   */
   public double hasTarget() {
     double value = limelightTableValue("tv"); //0 or 1
     return value;
   }
+  /**
+   * turn the leds on.
+   */
   public void setLedOn(){
     writeTableValue("ledMode", 3);
   }
+  /**
+   * turn leds off.
+   */
   public void setLedOff(){
     writeTableValue("ledMode", 1);
   }
+  /**
+   * set the leds to blink mode
+   * (seizure mode)
+   */
   public void setLedBlink(){
     writeTableValue("ledMode", 2);
   }
+  /**
+   * set the leds to the default value
+   */
   public void setLedDefault(){
     writeTableValue("ledMode", 0);
   }
+  /**
+   * set the camera mode to vision.
+   */
   public void setModeVision(){
     writeTableValue("camMode", 0);
   }
+  /**
+   * set the camera mode to drive. this increases exposure and disables vision tracking
+   */
   public void setModeDrive(){
     writeTableValue("camMode", 1);
   }
+  /**
+   * set the pipeline to filter the image from.
+   * @param pipe the pipeline number, from 0 - 9
+   */
   public void setPipeline(int pipe){
     writeTableValue("pipeline", pipe);
   }
